5f84352251e1503000ffed0bd8770f996adac1a7
Marco Bonici <bonici.marco@gmail.com>
2025-10-05 01:07:00 -0400

    Update test_background.py

--- tests/test_background.py
+++ tests/test_background.py
@@ -153,7 +153,7 @@ class TestCLASSComparison1:
 
         # Growth rate
         f1 = cosmo.f_z(z)
-        assert np.isclose(f1, 0.951063970660909, rtol=1e-4)  # 5% tolerance for growth rate
+        assert np.isclose(f1, 0.951063970660909, rtol=2e-4)
 
         # Hubble parameter
         H1 = cosmo.E_z(z) * 100 * cosmo.h
@@ -179,11 +179,11 @@ class TestCLASSComparison1:
 
         # Growth rate
         f2 = cosmo.f_z(z)
-        assert np.isclose(f2, 0.9763011446824891, rtol=1e-4)  # 5% tolerance for growth rate
+        assert np.isclose(f2, 0.9763011446824891, rtol=2e-4)
 
         # Hubble parameter
         H2 = cosmo.E_z(z) * 100 * cosmo.h
-        assert np.isclose(H2, 198.43712939715508, rtol=1e-4)
+        assert np.isclose(H2, 198.43712939715508, rtol=2e-4)
 
         # Comoving distance
         assert np.isclose(cosmo.r_z(z), 5815.253842752389, rtol=1e-4)
@@ -233,7 +233,7 @@ class TestCLASSComparison2:
 
         # Growth rate at z=0
         f0 = cosmo.f_z(z)
-        assert np.isclose(f0, 0.682532170290542, rtol=1e-4)  # 5% tolerance for growth rate
+        assert np.isclose(f0, 0.682532170290542, rtol=2e-5)
 
         # Hubble parameter H(z=0) = H0
         H0 = cosmo.E_z(z) * 100 * cosmo.h
@@ -255,7 +255,7 @@ class TestCLASSComparison2:
 
         # Growth rate
         f1 = cosmo.f_z(z)
-        assert np.isclose(f1, 0.9428198389771597, rtol=1e-4)  # 5% tolerance for growth rate
+        assert np.isclose(f1, 0.9428198389771597, rtol=2e-4)
 
         # Hubble parameter
         H1 = cosmo.E_z(z) * 100 * cosmo.h
@@ -281,11 +281,11 @@ class TestCLASSComparison2:
 
         # Growth rate
         f2 = cosmo.f_z(z)
-        assert np.isclose(f2, 0.981855910972107, rtol=1e-4)  # 5% tolerance for growth rate
+        assert np.isclose(f2, 0.981855910972107, rtol=2e-4)
 
         # Hubble parameter
         H2 = cosmo.E_z(z) * 100 * cosmo.h
-        assert np.isclose(H2, 224.06947149941828, rtol=1e-4)
+        assert np.isclose(H2, 224.06947149941828, rtol=2e-4)
 
         # Comoving distance
         assert np.isclose(cosmo.r_z(z), 5254.860436794502, rtol=1e-4)
